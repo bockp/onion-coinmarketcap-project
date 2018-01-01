3c093d27570c2113626ac7a841cbc1be67c9c7b7
Peter Bock <bockpeter@hotmail.com>
2018-01-01 12:36:14 +0100

    Tested the coin market API.
    python coinmarketcap API works fine.
    Added ability to display SmartCash, Steem and Ethereum, instead of just
    bitcoin.
    
    Still haven’t been able to test on Onion Omega2+, as it won’t connect
    to internet properly yet XS.

--- coinmarketcapMain.py
+++ coinmarketcapMain.py
@@ -19,9 +19,17 @@ if (ret != 0):
 # write a message
 
 coinmarketcapM = Market()
-eth-ticker = coinmarketcap.ticker("ethereum", limit=3, convert='EUR')
+eth-ticker = coinmarketcap.ticker("ethereum", limit=3)
+steem-ticker = coinmarketcap.ticker("steem", limit=3)
+smartcash-ticker = coinmarketcap.ticker("smartcash", limit=3)
 
-ret 	= oledExp.write("Ethereum price", eth-ticker[0]["price_eur"])
+ethUSD = eth-ticker[0]["price_usd"]
+steemUSD = steem-ticker[0]["price_usd"]
+smartcashUSD = smartcash-ticker[0]["price_usd"]
+
+ret 	= oledExp.write("Ethereum price: " + ethUSD + "$\n" +
+                        "Steem price: " +  steemUSD + "$\n" +
+                        "SmartCash price: " + smartcashUSD + "$\n")
 print "write return: ", ret
 if (ret != 0):
 	exit()
