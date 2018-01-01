1fcd0f27777d4626e317961f860cb0ebb93bcffd
Peter Bock <bockpeter@hotmail.com>
2018-01-01 19:37:37 +0100

    bug fix OLED display
    managed to connect to wifi !
    
    After a bug or two, it now displays the text, but as a single returning
    line.
    
    Gotta figure out how to create several lines ! (and shorten the text
    displayed… Tickers instead of names should do it !)

--- coinmarketcapMain.py
+++ coinmarketcapMain.py
@@ -18,22 +18,23 @@ if (ret != 0):
 
 # write a message
 
-coinmarketcapM = Market()
-eth-ticker = coinmarketcap.ticker("ethereum", limit=3)
-steem-ticker = coinmarketcap.ticker("steem", limit=3)
-smartcash-ticker = coinmarketcap.ticker("smartcash", limit=3)
+coinmarketcap = Market()
+eth_ticker = coinmarketcap.ticker("ethereum", limit=3)
+steem_ticker = coinmarketcap.ticker("steem", limit=3)
+smartcash_ticker = coinmarketcap.ticker("smartcash", limit=3)
 
-ethUSD = eth-ticker[0]["price_usd"]
-steemUSD = steem-ticker[0]["price_usd"]
-smartcashUSD = smartcash-ticker[0]["price_usd"]
+ethUSD = eth_ticker[0]["price_usd"]
+steemUSD = steem_ticker[0]["price_usd"]
+smartcashUSD = smartcash_ticker[0]["price_usd"]
 
 ret 	= oledExp.write("Ethereum price: " + ethUSD + "$\n" +
                         "Steem price: " +  steemUSD + "$\n" +
-                        "SmartCash price: " + smartcashUSD + "$\n")
+                        "SmartCash price: " + smartcashUSD + "$\n") # need to write these 3 one by one on separate lines ?
+                        # also, need to reduce num of significant digits...
 print "write return: ", ret
 if (ret != 0):
 	exit()
-time.sleep(4)
+time.sleep(10)
 
 
 # clear the display
